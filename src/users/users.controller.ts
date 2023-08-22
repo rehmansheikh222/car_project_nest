25ce555837b8423fb347ee8f610c3921b9f39390
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-22 17:17:53 +0500

    Handling exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -13,8 +13,12 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string) {
-        return this.userService.findOne(parseInt(id))
+    async findUser(@Param('id') id: string) {
+        const user = await this.userService.findOne(parseInt(id))
+        if (!user) {
+            throw new NotFoundException('User not found')
+        }
+        return user
     }
 
     @Get()
