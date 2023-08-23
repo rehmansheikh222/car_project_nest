956497a68edd9861c60aa7c8f72037da0d80fd80
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-23 20:02:30 +0500

    Creating a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,15 +4,16 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService){}
+    constructor(private userService: UsersService, private authService: AuthService){}
 
     @Post('/signup')
     createUser (@Body() body: CreateUserDto){
-        this.userService.create(body.email, body.password)
+        return this.authService.signup(body.email, body.password)
     }
 
     // @UseInterceptors(new SerializeInterceptor(UserDto))
