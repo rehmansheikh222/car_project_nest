9cd1d509cc62b14e305af6ba9006d66921de8fe4
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-23 22:48:21 +0500

    getting the current user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -21,6 +21,11 @@ export class UsersController {
         return session.color
     }
 
+    @Get('/whoami')
+    whoAmI(@Session() session : any) {
+        return this.userService.findOne(session.userId)
+    }
+
     @Post('/signup')
     async createUser (@Body() body: CreateUserDto, @Session() session: any){
         const user = await this.authService.signup(body.email, body.password)
