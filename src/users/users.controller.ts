3d98c34fa128d1b8a70026655ecfdad99439d5c8
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-25 18:34:02 +0500

    Custom Param Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,8 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -21,9 +23,14 @@ export class UsersController {
         return session.color
     }
 
+    // @Get('/whoami')
+    // whoAmI(@Session() session : any) {
+    //     return this.userService.findOne(session.userId)
+    // }
+
     @Get('/whoami')
-    whoAmI(@Session() session : any) {
-        return this.userService.findOne(session.userId)
+    whoAmI(@CurrentUser() user: string) {
+        return user
     }
 
     @Post('/signout')
